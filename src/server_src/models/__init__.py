4716abba4ceef9c593ec638013a8fbe7e64a6646
TrishinA <trishin.aoff@mail.ru>
2024-04-19 15:20:22 +0300

    feat: Добавил зависимости

--- src/server_src/models/__init__.py
+++ src/server_src/models/__init__.py
@@ -1,11 +1,13 @@
 __all__ = (
     "Base",
     "Product",
+    "Users",
     "DataBaseHelper",
-    "db"
+    "db_helper"
 )
 
 from .base import Base
 from .db_helper import DataBaseHelper
-from .db_helper import db 
+from .db_helper import db_helper
 from .product import Product
+from .users import Users
