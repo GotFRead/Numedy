5340d064667c10e8c80cca1212f0fe292cbd0cb4
TrishinA <trishin.aoff@mail.ru>
2024-04-19 14:51:24 +0300

    feat: Расширил db helper

--- src/server_src/models/db_helper.py
+++ src/server_src/models/db_helper.py
@@ -1,6 +1,9 @@
 
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import async_scoped_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from asyncio import current_task
 
 from core.config import setting
 
@@ -16,5 +19,19 @@ class DataBaseHelper:
             autocommit=False,
             expire_on_commit= False
         )
+    
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
 
-db = DataBaseHelper(echo=True)
+        return session
+    
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session 
+            await session.close()
+
+db_helper = DataBaseHelper(echo=True)
