8c1592d50c87b7b0b675ed35f22ccacbc8191ced
TrishinA <trishin.aoff@mail.ru>
2024-04-19 14:52:10 +0300

    feat: Добавил router-ы

--- src/server_src/main.py
+++ src/server_src/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 from server.routes import router as base_router
 from server.users.routes import router as user_router
+from server.warehouse.routes import router as product_router
 from models.base import Base
 from models.product import Product
 from models.db_helper import DataBaseHelper
-from models.db_helper import db
+from models.db_helper import db_helper
 
 
 from contextlib import asynccontextmanager
@@ -15,24 +16,26 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
-
-# __web_server__
-app = FastAPI()
-
-app.include_router(user_router)
-app.include_router(base_router)
-
+# __lifespan__
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db.engine.begin() as connection:
+    async with db_helper.engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all)
+        
 
     yield
 
+
+# __web_server__
+app = FastAPI(lifespan=lifespan, debug=True)
+
+app.include_router(user_router)
+app.include_router(base_router)
+app.include_router(product_router)
+
 def run_server():
-    uvicorn.run(f'main:app', host="0.0.0.0", port=8000,
-                reload=False, loop='asyncio')
+    uvicorn.run(f'main:app', host="0.0.0.0", port=8000, loop='asyncio')
 
 
 if __name__ == '__main__': 
