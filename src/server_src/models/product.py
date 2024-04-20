10a1066d7d8b4482363fd229aa89126007b4aa2b
TrishinA <trishin.aoff@mail.ru>
2024-04-20 22:26:08 +0300

    fix: Удалил старые модели

--- src/server_src/models/product.py
+++
@@ -1,9 +0,0 @@
-
-from .base import Base
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
-class Product(Base):
-    __tablename__ = "products"
-    name: Mapped[str]
-    weight: Mapped[int]
